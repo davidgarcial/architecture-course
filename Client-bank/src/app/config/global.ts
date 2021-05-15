cce75bf7dcc09c7e92d05bfba55f18483dbe44d2
davidgarcial <d.alex10092@gmail.com>
2021-05-15 18:48:47 -0500

    End course

--- Client-bank/src/app/config/global.ts
+++ Client-bank/src/app/config/global.ts
@@ -1,4 +1,4 @@
 export const Global = {
-    url: 'http://localhost:5000/api/'
-    //url: 'http://40.87.106.64:9000/api/'
+    url: 'http://52.170.84.73:9000/api/'
+    // url: 'http://localhost:5000/api/'
 };
